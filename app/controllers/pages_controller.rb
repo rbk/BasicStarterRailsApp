bda5f892110f8fd60e359a225905013e1925a958
richard <richard.be.jamin@gmail.com>
2014-10-03 11:19:40 -0500

    added page controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def signup
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Richard::Application.routes.draw do
 
+  get "pages/home"
+  get "pages/signup"
+  get "pages/about"
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
