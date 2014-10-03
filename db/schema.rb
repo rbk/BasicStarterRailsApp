68e4141172235632457d9feec953ec04b6ca1e92
richard <richard.be.jamin@gmail.com>
2014-10-03 11:25:09 -0500

    migrated database for sessions models

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141003032100) do
+ActiveRecord::Schema.define(version: 20141003162337) do
 
   create_table "groups", force: true do |t|
     t.string   "name"
     t.integer  "user_count"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sessions", force: true do |t|
+    t.string   "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
